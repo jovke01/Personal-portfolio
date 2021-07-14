62322f4066d5f4954b889a2b5f3f0b86e8b523cb
Pavle Jovanovic <jovanovicpavle00@gmail.com>
2021-07-14 17:57:16 +0200

    logo added

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,16 +2,20 @@ import React from 'react'
 import { Link } from 'gatsby'
 import '../styles/global.scss'
 import '../styles/nav.scss'
+import fullLogo from '../images/fullLogo.svg'
 
 export default function navbar() {
     return (
         <nav>
-            <p>Pavle Jovanović</p>
-            <ul>
-                <Link to="/"><li>Pocetna</li></Link>
-                <Link to="/oMeni"><li>O meni</li></Link>
-                <Link to="/kontakt"><li>Kontakt</li></Link>
-            </ul>
+            <div className="container">
+                <img src={fullLogo} alt="logo" />
+                {/* <p>Pavle Jovanović</p> */}
+                <ul>
+                    <Link to="/"><li>Pocetna</li></Link>
+                    <Link to="/oMeni"><li>O meni</li></Link>
+                    <Link to="/kontakt"><li>Kontakt</li></Link>
+                </ul>
+            </div>
         </nav>
     )
 }
