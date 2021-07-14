df78c81048effc84119c8629702f1682ffecda9b
Pavle Jovanovic <jovanovicpavle00@gmail.com>
2021-07-14 10:55:31 +0200

    header experement

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,7 +7,7 @@ export const Header = () => {
         <div className='header '>
             <div className="container">
                 <h1>Sve što je potrebno za vaš sajt na jednom mestu</h1>
-
+                <h2>Front-end web developer / dizajner</h2>
             </div>
         </div>
     )
