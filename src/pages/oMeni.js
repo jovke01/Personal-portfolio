06350af39e5a7ce3b47f9be871625e44537cde6f
Pavle Jovanovic <jovanovicpavle00@gmail.com>
2021-07-21 18:58:21 +0200

    footer spaceing fixed

--- src/pages/oMeni.js
+++ src/pages/oMeni.js
@@ -9,7 +9,7 @@ const oMeni = () => {
             <Navbar />
             <section className="omeni">
                 <div className="container">
-                    <h2>Ko sam ja i zaštobi ste želeli da radite sa mnom?</h2>
+                    <h2>Ko sam ja i zašto bi ste želeli da radite sa mnom?</h2>
                     <p className="dobrodoslica">Dobar dan, ja se zovem Pavle i zelim vam dobrodošlicu na moj web sajt.</p>
                     <div className="normal-txt">
                         <h3>Obrazovanje :</h3>
@@ -21,7 +21,7 @@ const oMeni = () => {
                     <div className="normal-txt">
                         <h3>Zašto se ovime bavim :</h3>
                         <p>Fasciniraju me dobar dizajn kao i to kako je taj dizajn inplementuran u kodu.
-                            Uživam u celom procesu izrade sajta i generalno volim interakciju sa ljudima koji su oni potrebni.
+                            Uživam u celom procesu izrade sajta.
                         </p>
                     </div>
                 </div>
