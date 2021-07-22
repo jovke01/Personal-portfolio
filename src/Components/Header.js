67d81595faef95aa82067db2fa46fb6ed1b5e2fc
Pavle Jovanovic <jovanovicpavle00@gmail.com>
2021-07-22 17:03:14 +0200

    header image commpres replace

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,21 +1,8 @@
 import React from 'react'
 import '../styles/header.scss'
-import { StaticImage } from "gatsby-plugin-image"
 export const Header = () => {
     return (
         <header className='header'>
-            <div style={{ display: "grid" }}>
-                <StaticImage
-                    style={{
-                        gridArea: "1/1",
-                    }}
-                    layout="fullWidth"
-                    aspectRatio={3 / 1}
-                    alt=""
-                    src={'../images/bg-min.jpg'}
-                    formats={["webp", "avif"]}
-                />
-            </div>
             <div className="container">
                 <h1>Sve što je potrebno za Vaš sajt na jednom mestu</h1>
                 <h2>Front-end web developer / dizajner</h2>
